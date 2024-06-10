35dcc3f0adc7fc9bbca7cfd2c50facfcdfe450f2
Javier0003 <jpere0500@gmail.com>
2024-06-09 22:14:12 -0400

    New: -now taking values of the images to open the modal

--- src/pages/main/components/body/components/image.tsx
+++ src/pages/main/components/body/components/image.tsx
@@ -1,19 +1,38 @@
 import { FileEntry } from '@tauri-apps/api/fs'
 import { convertFileSrc } from '@tauri-apps/api/tauri'
 
+type Modal = {
+  path: string
+  asset: string
+  name: string
+} | null
+
 type ImgProps = {
   val: FileEntry
   i: number
+  set: (val: Modal) => void
 }
-export default function ImgComp({ val, i }: ImgProps) {
+
+export default function ImgComp({ val, i, set }: ImgProps) {
   const assetUrl = convertFileSrc(val.path)
+
+  const handleClick = () =>{
+    if(!val.name) return
+
+    set({
+      path: val.path,
+      asset: assetUrl,
+      name: val.name
+    })
+  }
+
   return (
     <div
-      className="max-w-2/12 max-h-40 w-2/12 h-40 flex flex-col rounded-md bg-zinc-800"
-      key={`container_${i}`}
+      className="max-w-2/12 w-2/12 flex flex-col rounded-md bg-zinc-800 p-1"
+      key={`container_${i}`} onClick={handleClick}
     >
       <img src={assetUrl} key={`img_${i}`} className="max-w-2/12 max-h-40" />
-      {val.name}
+      <label htmlFor="">{val.name?.split(".png")[0]}</label>
     </div>
   )
 }
