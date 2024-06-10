d64e2053938e08d6ac8844a9c0af057800623f11
Javier0003 <jpere0500@gmail.com>
2024-06-09 22:19:01 -0400

    New: -added buttons to change between local and skin assets

--- src/pages/main/components/left-pannel/left-pannel.tsx
+++ src/pages/main/components/left-pannel/left-pannel.tsx
@@ -2,8 +2,26 @@ import Button from './components/button'
 
 const LeftPannel = (): JSX.Element => {
   return (
-    <div className="bg-slate-950 flex w-2/12 p-2 pt-16">
+    <div className="bg-slate-950 flex w-2/12 p-2 pt-16 flex-col justify-between">
       <Button>Select Path</Button>
+
+      <section className='flex flex-col gap-9'>
+        <div>
+          <label htmlFor="">Skin Assets</label>
+          <div className="flex w-full justify-between gap-1">
+            <button className="bg-slate-800">Circle</button>
+            <button className="bg-slate-800">Cursor</button>
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="">Your Assets</label>
+          <div className="flex w-full justify-between gap-1">
+            <button className="bg-slate-800">Circle</button>
+            <button className="bg-slate-800">Cursor</button>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
