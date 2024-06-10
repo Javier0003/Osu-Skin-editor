89eb1927f8a4deba0b5e94fb1e40c80537fa270e
Javier0003 <jpere0500@gmail.com>
2024-06-09 22:18:38 -0400

    Update: -changed the data structure of session Paths Removed: -getSessionPaths function (might change fetchData name to that in future)

--- src/store/sessionPaths.ts
+++ src/store/sessionPaths.ts
@@ -1,23 +1,39 @@
 import { FileEntry, readDir } from '@tauri-apps/api/fs'
 import { create } from 'zustand'
 
+type Sessions = {
+  cursor: FileEntry[]
+  circles: FileEntry[]
+  // localCursors: FileEntry[]
+  // localCircles: FileEntry[]
+}
+
 type SessPaths = {
-  sessionPath: FileEntry[]
-  getSessionPaths: (val: FileEntry[]) => void
-  fetchData: (
-    selectedPath: string
-  ) => void
+  sessionPath: Sessions
+  fetchData: (selectedPath: string) => void
 }
 
 export const sessionFilePath = create<SessPaths>((set) => ({
-  sessionPath: [],
-  getSessionPaths: (val) => {
-    set({ sessionPath: val })
+  sessionPath: {
+    cursor: [],
+    circles: [],
+    // localCursors: [],
+    // localCircles: []
   },
   fetchData: async (selectedPath) => {
     try {
       const compList = await readDir(selectedPath)
-      set({ sessionPath: compList })
+      const circles = compList.filter((file) => {
+        if (file.name?.includes('circle') && file.name?.includes('spinner'))
+          return
+        return file.name?.includes('circle')
+      })
+
+      const cursorFiles = compList.filter((file) =>
+        file.name?.startsWith('cursor')
+      )
+
+      set({ sessionPath: { cursor: cursorFiles, circles: circles } })
     } catch (error) {}
   }
 }))
