a24af69818555aee1a6d032f74f98ad87ecb08ed
Javier0003 <jpere0500@gmail.com>
2024-06-09 22:12:24 -0400

    New: -modal for removing files

--- src/pages/main/components/body/body.tsx
+++ src/pages/main/components/body/body.tsx
@@ -1,29 +1,36 @@
 import ImgComp from './components/image'
 import { sessionFilePath } from '../../../../store/sessionPaths'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { FilePath } from '../../../../store/path'
+import ModalComp from './components/modal'
+
+type Modal = {
+  path: string
+  asset: string
+  name: string
+} | null
 
 const Body = (): JSX.Element => {
+  const [ modal, setModal ] = useState<Modal>()
   const { sessionPath, fetchData } = sessionFilePath((state) => state)
   const { path } = FilePath((state) => state)
 
-  useEffect(() =>{
+  useEffect(() => {
     fetchData(path)
-  }, [])
+  }, [sessionPath])
 
   return (
     <div className="flex place-content-center pt-20 pl-4 pr-4 flex-wrap gap-1">
-      {sessionPath.map((val, index) => {
+      {modal && (
+        <ModalComp path={modal.path} asset={modal.asset} name={modal.name} close={setModal} />
+      )}
+
+      {sessionPath.cursor.map((val, index) => {
         const format = val.path.split('.').pop()
-        if (format === 'png') return <ImgComp val={val} i={index} key={`comp_${index}`}/>
-        
-        // if (format === 'ogg')
-        //   return (
-        //     <div className="w-2/12 h-40" key={`container_${index}`}>
-        //       <img src={assetUrl} alt="" key={`img_${index}`} />
-        //       {val.name}
-        //     </div>
-        //   )
+        if (format === 'png')
+          return (
+            <ImgComp val={val} i={index} key={`comp_${index}`} set={setModal} />
+          )
         return null
       })}
     </div>
